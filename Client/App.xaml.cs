f54c7c217648af4f78d8f54bf1a9fedb55f77ff9
川 彭 <z1476606543@outlook.com>
2018-09-19 20:37:57 +0800

    添加项目文件。

---
+++ Client/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DMSkin.WPF.API;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //初始化UI Dispatcher
+            Execute.InitializeWithDispatcher();
+
+            ShutdownMode = ShutdownMode.OnLastWindowClose;
+
+            //启动窗口
+            Windows.LoginWindow main = new Windows.LoginWindow();
+            main.Show();
+        }
+    }
+}
---
+++ Client/ViewModels/StartWindowViewModel.cs
@@ -0,0 +1,83 @@
+﻿using DMSkin;
+using DMSkin.WPF;
+using DMSkin.WPF.API;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Client.ViewModels
+{
+    public class StartWindowViewModel:ViewModelBase
+    {
+        //单层
+        public ICommand ComplexWindowCommand
+        {
+            get
+            {
+                return new DelegateCommand(obj =>
+                {
+                    //UI线程执行
+                    Execute.OnUIThread(() =>
+                    {
+                        //new ComplexWindow().Show();
+                    });
+                });
+            }
+        }
+        //双层带系统动画
+        public ICommand SimpleWindowCommand
+        {
+            get
+            {
+                return new DelegateCommand(obj =>
+                {
+                    //new SimpleWindow().Show();
+                });
+            }
+        }
+
+        private Color _DMWindowShadowColor;
+
+        /// <summary>
+        /// 窗体阴影颜色
+        /// </summary>
+        public Color DMWindowShadowColor
+        {
+            get { return _DMWindowShadowColor; }
+            set
+            {
+                _DMWindowShadowColor = value;
+                OnPropertyChanged("DMWindowShadowColor");
+            }
+        }
+
+        public ICommand ChangeWindowCommand
+        {
+            get
+            {
+                return new DelegateCommand(obj =>
+                {
+                    DMWindowShadowColor = (Color)ColorConverter.ConvertFromString(obj.ToString());
+                });
+            }
+        }
+
+        public ICommand OpenLinkCommand
+        {
+            get
+            {
+                return new DelegateCommand(obj =>
+                {
+                    if (obj is string url)
+                    {
+                        Process.Start(url);
+                    }
+                });
+            }
+        }
+    }
+}
